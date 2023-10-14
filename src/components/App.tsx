743864a4ac2d7c8d041a32a4f407905aa13a46e9
Nick Koukis <nikoskoukis99@gmail.com>
2023-10-14 19:23:08 +0300

    add jwt decoded

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
+import jwt_decode from 'jwt-decode'
 
 const GoogleLoginApp = () => {
   return (
     <GoogleOAuthProvider clientId='914628494898-onc9q6mppitoa680q721r42ki64hh556.apps.googleusercontent.com'>
       <GoogleLogin
         onSuccess={(credentialResponse) => {
-          console.log(credentialResponse)
+          const decoded = jwt_decode(credentialResponse as string)
+          console.log(decoded)
         }}
         onError={() => {
           console.log('Login Failed')
