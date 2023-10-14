94868e4e3b38ea7d75851f84bd1867300bf98524
Nick Koukis <nikoskoukis99@gmail.com>
2023-10-14 20:13:28 +0300

    try to render data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
 import jwt_decode from 'jwt-decode'
 
 const GoogleLoginApp = () => {
+  const [decodedData, setDecodedData] = useState<any | null>(null)
+
+  const handleSuccess = (credentialResponse: any) => {
+    console.log(credentialResponse)
+    const decoded = jwt_decode(credentialResponse.credential as string)
+    console.log(decoded)
+    setDecodedData(decoded)
+  }
+
   return (
-    <GoogleOAuthProvider clientId='914628494898-onc9q6mppitoa680q721r42ki64hh556.apps.googleusercontent.com'>
-      <GoogleLogin
-        onSuccess={(credentialResponse) => {
-          console.log(credentialResponse)
-          const decoded = jwt_decode(credentialResponse.credential as string)
-          console.log(decoded)
-        }}
-        onError={() => {
-          console.log('Login Failed')
-        }}
-      />
-    </GoogleOAuthProvider>
+    <div>
+      <GoogleOAuthProvider clientId='914628494898-onc9q6mppitoa680q721r42ki64hh556.apps.googleusercontent.com'>
+        <GoogleLogin
+          onSuccess={handleSuccess}
+          onError={() => {
+            console.log('Login Failed')
+          }}
+        />
+      </GoogleOAuthProvider>
+
+      {decodedData && (
+        <div>
+          <h2>Decoded Data</h2>
+          <pre>{JSON.stringify(decodedData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
   )
 }
 
